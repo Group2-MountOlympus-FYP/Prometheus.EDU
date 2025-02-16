9ef0040f33ee3ab1f9766b59b78e7e3cc92c08a9
Sandoleathy <3296446003@qq.com>
2025-02-16 17:51:11 +0800

    删除了旧代理，增加了新代理

--- next.config.mjs
+++ next.config.mjs
@@ -19,17 +19,9 @@ export default withBundleAnalyzer({
     async rewrites() {
         return [
             {
-                source: '/zhhq/:path*', // 匹配所有 /zhhq 开头的路径
-                destination: 'https://www.cneeex.com/zhhq/:path*', // 代理到目标地址
-            },
-            {
-                source: '/jsonData/:path*', // 匹配所有 /zhhq 开头的路径
-                destination: 'https://www.cneeex.com/zhhq/jsonData/:path*', // 代理到目标地址
-            },
-            {
-                source: '/img/:path*', // 匹配所有 /zhhq 开头的路径
-                destination: 'https://www.cneeex.com/img/:path*', // 代理到目标地址
-            },
+                source: '/:path*',
+                destination: 'http://127.0.0.1:5000/:path*'
+            }
         ];
     },
 });
