50332ab7a7ec089e22f10fbaa034a3b0cd95181e
Sandoleathy <3296446003@qq.com>
2025-02-27 11:36:30 +0800

    设置了新的后端蓝图以及开发了视频传送和播放功能

--- components/VideoPlayPanel/VideoPlayer.jsx
+++ components/VideoPlayPanel/VideoPlayer.jsx
@@ -1,13 +1,21 @@
 'use client'
+import React, { useState, useEffect } from 'react';
+import { GetVideo } from '@/app/api/Video/router';
 
 export function VideoPlayer(){
+    const [videoUrl, setVideoUrl] = useState('');
+
+    const getTestVideo = async () => {
+        const response = await GetVideo('test.mp4')
+        console.log(response)
+    }
     return (
         <div>
-            <p>Hello</p>
-            <video width="640" height="360" controls>
-                <source src='/video/test.mp4' type='video/mp4'></source>
-                Your browser does not support the video tag.
+            <video controls width="100%">
+            <source src='http://127.0.0.1:5000/video/test.mp4' type="video/mp4" />
+            Your browser does not support the video tag.
             </video>
+            <button onClick={getTestVideo}>Get Testing Video</button>
         </div>
     )
 }
--- next.config.mjs
+++ next.config.mjs
@@ -22,7 +22,7 @@ export default withBundleAnalyzer({
             {
                 source: '/:path*',
                 destination: 'http://127.0.0.1:5000/:path*'
-            }
+            },
         ];
     },
 });
