2f66c728834b87a64bb10c49a72f26610968b5dd
Sandoleathy <3296446003@qq.com>
2025-03-16 22:00:49 +0800

    富文本编辑器以及新安装的库

--- app/PostTest/page.tsx
+++ app/PostTest/page.tsx
@@ -1,28 +1,12 @@
 'use client';
 
 import { useEffect, useState } from 'react';
+import { RichTextEditor } from '@/components/RichTextEditor/RichTextEditor';
 
 export default function PostTest() {
-  const [ReactQuill, setReactQuill] = useState<any>(null);
-  const [value, setValue] = useState<string>('');
-
-  useEffect(() => {
-    // 动态加载 react-quill 只在客户端
-    import('react-quill').then((module) => {
-      setReactQuill(() => module.default); // 注意这里是函数形式
-    });
-  }, []);
-
-  return (
+  return(
     <div>
-      <h1>发布文章</h1>
-
-      {/* 等待 ReactQuill 动态导入完成之后再渲染 */}
-      {ReactQuill ? (
-        <ReactQuill value={value} onChange={setValue} theme="snow" />
-      ) : (
-        <p>加载编辑器中...</p>
-      )}
+      <RichTextEditor/>
     </div>
-  );
+  )
 }
