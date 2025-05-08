8ce52ee0e633cc428f5a41244724ab935523ed2e
Lzyyy12 <ella.tian325@gmail.com>
2025-05-08 16:19:29 +0800

    点击头像跳转到profile功能

--- app/course/[courseId]/component/teachers_list.tsx
+++ app/course/[courseId]/component/teachers_list.tsx
@@ -1,5 +1,6 @@
 "use client";
 import React from "react";
+import { useRouter } from "next/navigation";
 import { Avatar, Text, Group, Stack, Title, Container } from "@mantine/core";
 import { getText } from "./language";
 
@@ -12,20 +13,30 @@ interface LecturerListProps {
   lecturers: Lecturer[];
 }
 
-const LecturerList: React.FC<LecturerListProps> = ({ lecturers }) => {
+export function LectureListForCourseDetail(props: LecturerListProps) {
+  const router = useRouter();
+
   return (
-    <>
-    <Container style={{ minWidth: "25vw", maxWidth: "25vw" }}>
-      <Title order={3} fw={500} style={{ marginBottom: '25px' }}>
-                {getText('Instructor_List')}
-      </Title>
-      <Group wrap="wrap" gap="xl">
-        {lecturers.map((lecturer, index) => {
+    <div>
+      <Group gap="xl" justify="space-between">
+        {props.lecturers.map((lecturer, index) => {
           const name = lecturer.username || getText("unknown_user");
           const avatarSrc = lecturer.avatar || undefined;
+
+          const toAuthorProfile = () => {
+            if (lecturer.username) {
+              router.push(`/Profile/${lecturer.username}`);
+            }
+          };
+
           return (
             <Stack align="center" key={index} gap={4}>
-              <Avatar radius="xl" src={avatarSrc}>
+              <Avatar
+                radius="xl"
+                src={avatarSrc}
+                onClick={toAuthorProfile}
+                style={{ cursor: "pointer" }}
+              >
                 {name[0]?.toUpperCase() || "?"}
               </Avatar>
               <Text>{name}</Text>
@@ -33,31 +44,6 @@ const LecturerList: React.FC<LecturerListProps> = ({ lecturers }) => {
           );
         })}
       </Group>
-      </Container>
-    </>
+    </div>
   );
-};
-
-export function LectureListForCourseDetail(props: LecturerListProps){
-  return (
-      <div>
-        <Group gap="xl" justify={"space-between"}>
-          {props.lecturers.map((lecturer, index) => {
-            const name = lecturer.username || getText("unknown_user");
-            const avatarSrc = lecturer.avatar || undefined;
-            return (
-                <Stack align="center" key={index} gap={4}>
-                  <Avatar radius="xl" src={avatarSrc}>
-                    {name[0]?.toUpperCase() || "?"}
-                  </Avatar>
-                  <Text>{name}</Text>
-                </Stack>
-            );
-          })}
-        </Group>
-      </div>
-  )
 }
-
-export default LecturerList;
-
